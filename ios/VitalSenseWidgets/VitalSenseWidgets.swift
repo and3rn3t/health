47f4099faf7a607cfa592f3f23a39e9d27edebe8
andernet <and3rn3t@icloud.com>
2025-11-01 16:23:35 -0500

    fix(ios): Add missing ConfigurationAppIntent definition for VitalSenseWidgets

--- ios/VitalSenseWidgets/VitalSenseWidgets.swift
+++ ios/VitalSenseWidgets/VitalSenseWidgets.swift
@@ -7,6 +7,15 @@
 
 import WidgetKit
 import SwiftUI
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource = "Configuration"
+    static var description = IntentDescription("This is an example widget.")
+
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
 
 struct Provider: AppIntentTimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
